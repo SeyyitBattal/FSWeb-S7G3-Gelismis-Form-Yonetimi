c0b5635f2565860926eae9f21370313b42b8dd4a
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-08-11 09:52:34 +0300

    Tests are done

--- kullanici-katilimi/cypress/e2e/form.cy.js
+++ kullanici-katilimi/cypress/e2e/form.cy.js
@@ -7,22 +7,34 @@ describe("Login Formu Testi", () => {
   });
 
   it("İsim Testi", () => {
-    cy.get("#exampleText").type("aaa");
+    cy.get("#exampleText").type("aa");
+    cy.get(".invalid-feedback").should(
+      "have.text",
+      "İsmin 3 harften kısa olamaz herhalde"
+    );
   });
 
   it("Email Testi", () => {
-    cy.get("#exampleEmail").type("aaa@aaaa.com");
+    cy.get("#exampleEmail").type("aaaa");
+    cy.get(".invalid-feedback").should(
+      "have.text",
+      "Geçerli bir mail girer misin?"
+    );
   });
 
   it("Şifre Testi", () => {
-    cy.get("#examplePassword").type("123456");
+    cy.get("#examplePassword").type("12356");
+    cy.get(".invalid-feedback").should(
+      "have.text",
+      "Güçlü parola hayat kurtarır"
+    );
   });
 
   it("Koşullar Testi", () => {
-    cy.get("#checkbox2").check();
+    cy.get("#checkbox2").check().should("be.checked");
   });
 
   it("Buton Testi", () => {
-    cy.get("[data-cy=login-button]").should("be.enabled");
+    cy.get("[data-cy=login-button]").should("be.disabled");
   });
 });
